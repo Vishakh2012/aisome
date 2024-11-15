6362840d9884ada3873a79aebd73d31df6f86623
vishakh prakash <vishakhpro2002@gmail.com>
2024-11-15 14:28:09 +0530

    FEAT: error in how function was called from neovim

--- lua/aisome/client.lua
+++ lua/aisome/client.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.request_suggestions()
     local line = vim.api.nvim_get_current_line()
-    local suggestions = vim.fn.getcompletion(line)
+    local suggestions = vim.fn['getcompletions'](line)
     vim.api.nvim_buf_set_extmark(0, 0, 0, 0, {
         virt_text = { { table.concat(suggestions, " "), "Comment" } },
         virt_text_pos = "eol",
